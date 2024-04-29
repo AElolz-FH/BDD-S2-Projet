0f84c728d9bb9e8b9b82195c070a78a7493dce0c
dylan72 <dylan.bouttefeux@hotmail.com>
2024-04-29 21:52:52 +0200

    Correction Post Seance + Fix GetSalleNumAndBat

--- salles/src/main/java/fr/uphf/formations/ressources/SalleRessource.java
+++ salles/src/main/java/fr/uphf/formations/ressources/SalleRessource.java
@@ -74,9 +74,9 @@ public class SalleRessource {
 
 
     @GetMapping("/numeroSalle={numeroSalle}/batiment={batiment}")
-    public ResponseEntity<getSalleByNumAndBatDTOOutput> getSalleByNumAndBat(@PathVariable(required = true) Integer numeroSalle,@PathVariable(required = true) String batiment) throws SalleNotFoundException {
-        Salles salle = this.salleRepository.findByNumeroSalleAndBatiment(numeroSalle,batiment);
-        if(salle.getNumeroSalle() != null || salle.getNumeroSalle() != 0 && (salle.getBatiment() != null || salle.getBatiment().equals(''))){
+    public ResponseEntity<getSalleByNumAndBatDTOOutput> getSalleByNumAndBat(@PathVariable(required = true) Integer numeroSalle, @PathVariable(required = true) String batiment) throws SalleNotFoundException {
+        Salles salle = this.salleRepository.findByNumeroSalleAndBatiment(numeroSalle, batiment);
+        if (salle.getNumeroSalle() != null && salle.getNumeroSalle() != 0 && salle.getBatiment() != null && !salle.getBatiment().isEmpty()) {
             getSalleByNumAndBatDTOOutput output = getSalleByNumAndBatDTOOutput.builder()
                     .nomSalle(salle.getNomSalle())
                     .numeroSalle(String.valueOf(salle.getNumeroSalle()))
@@ -88,7 +88,7 @@ public class SalleRessource {
         }
         return ResponseEntity.badRequest().body(getSalleByNumAndBatDTOOutput.builder()
                 .message("La salle n'a pas été trouvée")
-                .build();
+                .build());
     }
 
 
